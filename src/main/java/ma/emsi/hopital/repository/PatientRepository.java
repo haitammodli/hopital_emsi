3d8893d8920a402d18654250311c21f171a3b4f0
HaitamP <haytam.modli2004@gmail.com>
2025-04-14 10:04:33 +0100

    v1

---
+++ src/main/java/ma/emsi/hopital/repository/PatientRepository.java
@@ -0,0 +1,19 @@
+package ma.emsi.hopital.repository;
+
+import ma.emsi.hopital.entities.Patient;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+
+
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Page<Patient> findByNomContains(String keyword, PageRequest pageable);
+
+    @Query("select p from Patient p where p.nom like :x")
+    Page<Patient> chercher(@Param("x") String keyword, Pageable pageable);
+}
